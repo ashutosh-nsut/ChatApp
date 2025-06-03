242c58f3e148f2e8c743da2bff44e10426656319
Ashutosh Verma <ashutosh.verma.ug22@nsut.ac.in>
2025-06-03 22:32:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'http://localhost:3000, "https://chat-app-blue-phi-26.vercel.app"',
     credentials:true
 };
 app.use(cors(corsOption)); 
